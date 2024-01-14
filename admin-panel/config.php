859640f2e9473955618872baacd3b4342b3db03c
Mohamed AbdElkader <114310926+mabdo5@users.noreply.github.com>
2024-01-14 19:53:42 +0200

    Update config.php

--- admin-panel/config.php
+++ admin-panel/config.php
@@ -2,19 +2,19 @@
 // MySQL Hostname
 $sql_db_host = "";
 // MySQL Database User
-$sql_db_user = "raanh_aanh95461954";
+$sql_db_user = "raanh_test";
 // MySQL Database Password
-$sql_db_pass = "l~;!XHG+qlE$";
+$sql_db_pass = "test";
 // MySQL Database Name
-$sql_db_name = "raanh_raanh95461";
+$sql_db_name = "raanh_test";
 
 // Site URL
-$site_url = "https://raanh.com"; // e.g (http://example.com)
+$site_url = ""; // e.g (http://example.com)
 
 $auto_redirect = true;
 
 // Purchase code
-$purchase_code = "072442cc2434e17d8398"; // Your purchase code, don't give it to anyone.
+$purchase_code = ""; // Your purchase code, don't give it to anyone.
 
-$siteEncryptKey = "c435a47676229072442cc2434e17d83989b07d52"; // Your site encrypt key, don't give it to anyone.
+$siteEncryptKey = "c435a47676229072442ccb07d52"; // Your site encrypt key, don't give it to anyone.
 ?>
